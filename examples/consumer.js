a9e74dc3edf10d162821ab0d2aa21c64a8687f6a
Matthias Goetzke <m.goetzke@curasystems.de>
2014-03-11 10:13:26 +0100

    minimal change to add /open/close reliable fetch

--- examples/consumer.js
+++ examples/consumer.js
@@ -1,17 +1,31 @@
-var kestrel = require('../');
+'use strict';
+
+var kestrel = require('..');
 
 var consumer = new kestrel.kestrelConsumer( 'test', {
-    connectionType: kestrel.connectionType.ROUND_ROBIN,
-    servers: ['127.0.0.1:22133']
+  connectionType: kestrel.connectionType.ROUND_ROBIN,
+  servers: ['127.0.0.1:22133']
 });
 
-function consumed(message){
-    console.log('Consumed Message:');
-    console.dir(message);
+var counter = 0;
+
+function consumed(message,cb){
+  console.log('Consumed Message:');
+  console.dir(message);
+
+  if(counter++>1){
+    //produce failure;
+    return process.exit(1);
+  }
+
+  if(cb){
+    cb();
+  }
 }
 
-consumer.consume( consumed );
+consumer.consume( {reliable:true}, consumed );
+//unreliable still possible as before: consumer.consume( consumed );
 
 setTimeout( function(){
-    consumer.stopConsuming();
+  consumer.stopConsuming();
 }, 6000);
